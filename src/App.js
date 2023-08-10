1424a11ae029bc74b1412bbc48cb04a4c71e0610
Dhairya Chheda <dhairyakchheda@gmail.com>
2023-08-10 23:09:59 +0530

    Finishing the app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,61 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
+import "./index.css";
 import Header from "./components/ui/Header";
 import QuotesGrid from "./quotes/QuotesGrid";
 
 const App = () => {
   const [quotes, setQuotes] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [quotesNumber, setQuotesNumber] = useState("");
 
-  useEffect(() => {
-    const fetchQuotes = async () => {
-      const result = await axios.get(
-        `https://api.breakingbadquotes.xyz/v1/quotes/3`
-      );
-      setQuotes(result.data);
-      setIsLoading(false);
-    };
-    fetchQuotes();
-  }, []);
+  const fetchQuotes = async (quotesNumber) => {
+    setIsLoading(true);
+    const result = await axios.get(
+      `https://api.breakingbadquotes.xyz/v1/quotes/${quotesNumber}`
+    );
+    console.log(quotes);
+    setQuotes(result.data);
+    setIsLoading(false);
+  };
+
+  const isValidInput = (input) => {
+    return /[a-zA-Z]/.test(input);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (isValidInput(quotesNumber)) {
+      alert("Please enter number greater than 0");
+      setQuotesNumber("");
+      return;
+    }
+    fetchQuotes(quotesNumber);
+    setQuotesNumber("");
+  };
 
   return (
-    <div className="container">
+    <div className="container pb-16">
       <Header />
+      <form
+        onSubmit={onSubmit}
+        className=" w-full flex items-center justify-center my-8"
+      >
+        <input
+          type="text"
+          min="1"
+          className="border rounded-l-md py-2 px-3 w-1/2 text-green-900 placeholder:text-md outline-none"
+          placeholder="Number of quotes"
+          value={quotesNumber}
+          onChange={(e) => setQuotesNumber(e.target.value)}
+        />
+        <input
+          type="submit"
+          value="Generate"
+          className="bg-green-800 hover:bg-green-700 text-white text-lg rounded-r-md py-2 px-4"
+        />
+      </form>
       <QuotesGrid isLoading={isLoading} quotes={quotes} />
     </div>
   );
